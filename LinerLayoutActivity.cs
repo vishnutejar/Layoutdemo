26873c79614b96ef99c1a903ef66ef67ef70edd6
vishnu <vishnudevxamarin.gmail.com>
2021-05-20 21:34:42 +0530

    Add project files.

---
+++ LinerLayoutActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Layoutdemo
+{
+    [Activity(Label = "LinerLayoutActivity", MainLauncher = false)]
+    public class LinerLayoutActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.layout_linear);
+
+        }
+    }
+}
